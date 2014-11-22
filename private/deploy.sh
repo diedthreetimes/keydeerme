9bc1b1778a32bc43151098cad32e74d207e6dc8e
Sky Faber <sky.faber@gmail.com>
2014-11-22 10:15:00 -0800

    First Commit
    -Working deploy script

---
+++ private/deploy.sh
@@ -0,0 +1,13 @@
+#/usr/bin/bash
+# Note: This deploy script assumes the same username on the host
+# Its surprising a similar deploy script doesn't exist already
+meteor build .
+scp keydeerme.tar.gz keydeer.me:~
+# The way we move the bundle probably kills any seamless deploy. 
+#  There must be a better way
+#  Maybe look at : https://github.com/arunoda/meteor-up ?
+#  Also of potential interest is https://github.com/onmodulus/demeteorizer.
+#  This seems to require a change in how meteor is served.
+ssh keydeer.me 'tar -zxf keydeerme.tar.gz; rm -rf /var/www/keydeer.me; mv bundle /var/www/keydeer.me; cd /var/www/keydeer.me/programs/server && npm install; cd ../../; mkdir tmp; mv programs/web.browser/app public; touch tmp/restart.txt'
+
+# Todo we probably need to touch restart also 
